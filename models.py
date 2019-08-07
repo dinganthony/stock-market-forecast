b3bdea17eb94cbe817aa1decec27d7eccd0cbbe7
Anthony Ding <anthonyding@berkeley.edu>
2019-08-06 18:28:04 -0700

    Added training data, NB model

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
+'''models.py contains the classifier models called by the scripts.'''
+
 import csv
 import numpy as np
+from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 import tensorflow as tf
 from tensorflow import keras 
 import matplotlib as mpl
@@ -8,7 +14,8 @@ from matplotlib import pyplot as plt
 
 # Constants
 DEBUG_MODE = True
-
+NB_ALPHA = 1.0
+TFIDF_FEATURES = 10000
 
 '''The base SentimentClassifier class contains core logic 
 for different models of stock classifiers.'''
@@ -21,28 +28,36 @@ class SentimentClassifier:
             print("Reading from file:", file)
         stock_data = open(file)
         reader = csv.reader(stock_data)
-        self.data = self.process_data(reader)
+        self.data = self.preprocess_data(reader)
         self.file = file
         if self.debug:
-            print("Processed data:")
+            print("Preprocessed data:")
             print(self.data)
+        self.create_model()
 
-    def process_data(self, reader):
+    def preprocess_data(self, reader):
         raise NotImplementedError("To be implemented in subclasses")
     
     def create_model(self):
         raise NotImplementedError("To be implemented in subclasses")
     
-    def classify(self):
+    def classify(self, text):
         raise NotImplementedError("To be implemented in subclasses")
+
+
+''' The NaiveBayesClassifier uses a Bayes Net and the 'naive' assumption, 
+that features in article headlines such as word frequencies are independent
+of each other, to classify stock market movement.'''
 
 class NaiveBayesClassifier(SentimentClassifier):
 
-    def __init__(self, file, debug=DEBUG_MODE):
+    def __init__(self, file, debug=DEBUG_MODE, alpha=NB_ALPHA):
         super.__init__(file, debug)
+        self.alpha = alpha
         self.vocabulary = set()
 
-    def process_data(self, reader):
+    # TODO: y values should be ints not strings when read from CSV
+    def preprocess_data(self, reader):
         x_words = []
         y_values = []
         first_row_header = True
@@ -54,43 +69,66 @@ class NaiveBayesClassifier(SentimentClassifier):
             y_values.append(row[1])
             words_in_row = []
             for headline in row[2:]:
-                words = self.createVocabulary(headline)
+                words = self.create_vocabulary(headline)
                 words_in_row.extend(words)
             x_words.append(words_in_row)
-        data = zip(x_words, y_values)
-        np.random.shuffle(data)
-        return data
+        return (x_words, y_values)
                 
-    def createVocabulary(self, text):
+    def create_vocabulary(self, text):
         words = text.lower().replace(",", "").replace(".", "").replace("!", "")\
         .replace("?", "").replace(";", "").replace(":", "").replace("*", "")\
         .replace("(", "").replace(")", "").replace("/", "").split()
+        # Filter out stopwords (e.g. "the", "an")
+        stop = set(stopwords.words('english'))
         for word in words:
-            self.vocabulary.add(word)
+            if word not in stop:
+                self.vocabulary.add(word)
+        self.vocabulary = words
         return words
 
     def create_model(self):
-        x_train, y_train, x_test, y_test = train_test_split(self.data, test_size=0.2)
-
-    def _train_classify(self):
-        raise NotImplementedError()
-
-    def _validate_classify(self):
-        raise NotImplementedError()
-
-    def _test_classify(self):
-        raise NotImplementedError()
+        # Split data into sets for training and testing
+        x_train, x_test, y_train, y_test =\
+            train_test_split(self.data[0], self.data[1], test_size=0.2)
+        # Use tfidf to vectorize data
+        tfidfv = TfidfVectorizer(max_features=TFIDF_FEATURES)
+        tfidfv.fit(self.vocabulary)
+        self.tfidf_vocab = tfidfv.vocabulary
+        self.tfidfv = tfidfv
+        x_train_tfidf = tfidfv.transform(x_train)
+        x_test_tfidf = tfidfv.transform(x_test)
+        # Fit MultinomialNB to training data
+        self.nb = MultinomialNB(alpha=self.alpha)
+        self.nb.fit(x_train_tfidf, y_train)
+        # Test accuracy of NaiveBayesClassifier in predicting
+        self.testing = self.nb.predict(x_test_tfidf)
+        if DEBUG_MODE:
+            print("Testing classifier accuracy:", accuracy_score(self.testing, y_test) * 100)
     
-    def classify(self):
+    def classify(self, text):
+        words = self.create_vocabulary(text)
+        words = self.tfidfv.transform(words)
+        return self.nb.predict(words)
+
+
+'''These classifiers are a work in progress and will be implemented after the NaiveBayesClassifier.'''
+
+class SVMClassifier(NaiveBayesClassifier):
+
+    def create_model(self):
         raise NotImplementedError()
+
+    def classify(self, text):
+        raise NotImplementedError()
+
 
 class DeepLearningClassifier(SentimentClassifier):
 
-    def process_data(self, reader):
+    def preprocess_data(self, reader):
         raise NotImplementedError()
 
     def create_model(self):
         raise NotImplementedError()
 
-    def classify(self):
+    def classify(self, text):
         raise NotImplementedError()
--- script.py
+++ script.py
@@ -1,1 +1,26 @@
 '''script.py contains the main running block for sentiment analysis'''
+
+import models
+
+# Globals / soon-to-be-args
+DATA_FILE = 'Combined_News_DJIA.csv'
+TYPE = 'NaiveBayes'
+
+def main(clf_type):
+    test_headline = "China warns Hong Kong protesters not to 'play with fire'"
+    if clf_type == 'NaiveBayes':
+        classifier = models.NaiveBayesClassifier(DATA_FILE, alpha=1.0)
+    elif clf_type == 'SVM':
+        classifier = models.SVMClassifier(DATA_FILE, alpha=1.0)
+    elif clf_type == 'DeepLearning':
+        classifier = models.DeepLearningClassifier(DATA_FILE)
+    else:
+        raise ValueError("Not a valid classifier type.")
+    label = classifier.classify(test_headline)
+    if label:
+        print("Prediction: market will rise")
+    else:
+        print("Prediction: market will fall")
+    
+if __name__ == "__main__":
+    main(TYPE)
